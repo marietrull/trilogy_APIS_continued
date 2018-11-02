0ceaab84fd5080e92116e10b8298c58595438ace
marietrull <marietrull@gmail.com>
2018-11-01 21:25:48 -0500

    Modified Data Display

--- app.js
+++ app.js
@@ -40,22 +40,64 @@ console.log("Javascript Connected");
     renderMovies();
   });
 
-	//-----AJAX CALL FOR MOVIE DATA-----//
-	function displayMovieInfo() {
+//-----AJAX CALL FOR MOVIE DATA-----//
+    function displayMovieInfo() {
+   	$("#movies-view").empty();
 
-	var movie = $(this).attr("data-name");
-	var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+    var movie = $(this).attr("data-name");
+    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
-	$.ajax({
-		  url: queryURL,
-		  method: "GET"
-		}).then(function(response) {
-		  $("#movies-json").text(JSON.stringify(response));
-		});
-	}
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
 
-	//Display movie data on click
-	$(document).on("click", ".movie", displayMovieInfo);
+      // Creating a div to hold the movie
+      var movieDiv = $("<div class='movie'>");
 
-  // Calling the renderButtons function at least once to display the initial list of movies
-  renderMovies();
+      // Storing the rating data
+      var rating = response.Rated;
+
+      // Creating an element to have the rating displayed
+      var pOne = $("<p>").text("Rating: " + rating);
+
+      // Displaying the rating
+      movieDiv.append(pOne);
+
+      // Storing the release year
+      var released = response.Released;
+
+      // Creating an element to hold the release year
+      var pTwo = $("<p>").text("Released: " + released);
+
+      // Displaying the release year
+      movieDiv.append(pTwo);
+
+      // Storing the plot
+      var plot = response.Plot;
+
+      // Creating an element to hold the plot
+      var pThree = $("<p>").text("Plot: " + plot);
+
+      // Appending the plot
+      movieDiv.append(pThree);
+
+      // Retrieving the URL for the image
+      var imgURL = response.Poster;
+
+      // Creating an element to hold the image
+      var image = $("<img>").attr("src", imgURL);
+
+      // Appending the image
+      movieDiv.append(image);
+
+      // Putting the entire movie above the previous movies
+      $("#movies-view").prepend(movieDiv);
+    });
+}
+
+//Display movie data on click
+$(document).on("click", ".movie", displayMovieInfo);
+
+// Calling the renderButtons function at least once to display the initial list of movies
+renderMovies();
