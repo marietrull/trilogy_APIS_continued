1ed0468201ff291618a5b3c510e37dd1a061ac95
marietrull <marietrull@gmail.com>
2018-11-01 20:02:06 -0500

    Updated Comments + Users can Add New Divs

--- app.js
+++ app.js
@@ -12,19 +12,33 @@ console.log("Javascript Connected");
     // Looping through the array of movies
     for (var i = 0; i < movies.length; i++) {
 
-      // Then dynamicaly generating buttons for each movie in the array.
-      // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+      // Dynamicaly generating a new div for each movie in the array.
       var a = $("<div>");
-      // Adding a class
+      // Adding a class to the new div
       a.addClass("movie");
       // Adding a data-attribute with a value of the movie at index i
       a.attr("data-name", movies[i]);
-      // Providing the button's text with a value of the movie at index i
+      // Providing the div's text with a value of the movie at index i
       a.text(movies[i]);
-      // Adding the button to the HTML
+      // Adding the div to the HTML
       $("#movies-view").append(a);
     }
   }
+
+  // This function handles events where one div is clicked
+  $("#add-movie").on("click", function(event) {
+    // event.preventDefault() prevents the form from trying to submit itself.
+    // We're using a form so that the user can hit enter instead of clicking the button if they want
+    event.preventDefault();
+
+    // This line will grab the text from the input box
+    var movie = $("#movie-input").val().trim();
+    // The movie from the textbox is then added to our array
+    movies.push(movie);
+
+    // calling renderButtons which handles the processing of our movie array
+    renderMovies();
+  });
 
   // Calling the renderButtons function at least once to display the initial list of movies
   renderMovies();
